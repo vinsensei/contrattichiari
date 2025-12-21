d89ffebad6e93ccc96bfcc900ca4319ab674e101
Vincenzo <vincenzo@migliano.it>
2025-12-21 16:17:58 +0100

    aggiunta favicons per serp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,7 @@ export const metadata: Metadata = {
     icon: [
       { url: "/favicon.ico" },
       { url: "/favicon-48.png", sizes: "48x48", type: "image/png" },
-      { url: "/favicon-192.png", sizes: "192x192", type: "image/png" },
-      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon-192.png", sizes: "192x192", type: "image/png" }
     ],
     apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
   },
